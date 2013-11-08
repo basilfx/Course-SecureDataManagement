51ea90ab830996c7b5977b0872b3d88d7081e410
Erwin <erwinmiddelesch@gmail.com>
2013-11-08 18:31:17 +0100

    javascript search

--- search/static/custom-js/transaction_index.js
+++ search/static/custom-js/transaction_index.js
@@ -8,5 +8,11 @@ $(document).ready(function() {
 		var transaction = JSON.parse(data[i]["data"]);
 		addRow(id, transaction["sender"], transaction["receiver"], transaction["amount"], transaction["description"]);
 	}
-
+	$('input.filter').on('keyup', function() {
+    var rex = new RegExp($(this).val(), 'i');
+    $('.searchable tr').hide();
+        $('.searchable tr').filter(function() {
+            return rex.test($(this).text());
+        }).show();
+    });
 });

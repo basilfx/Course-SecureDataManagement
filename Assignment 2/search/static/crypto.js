9a16cf761044e8e89dab4fd4208a7a2143d73b84
Bas Stottelaar <basstottelaar@gmail.com>
2013-11-26 20:37:09 +0100

    Less code is more better

--- Assignment 2/search/static/crypto.js
+++ Assignment 2/search/static/crypto.js
@@ -1,15 +1,11 @@
 // This class is used for encryption/decryption
 var Crypto = function(key) {
-	this.key = key;
-	this.cipher = CryptoJS.AES;
-	
 	this.decrypt = function(ciphertext) {
-		return JSON.parse(CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(window.atob(ciphertext), this.key)));
+		return JSON.parse(CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(atob(ciphertext), key)));
 	}
 
 	this.encrypt = function(jsObj) {
-		console.log(jsObj);
-		return window.btoa(CryptoJS.AES.encrypt(JSON.stringify(jsObj), this.key).toString());
+		return btoa(CryptoJS.AES.encrypt(JSON.stringify(jsObj), key).toString());
 	}
 
 	this.bucket = function(input) {
